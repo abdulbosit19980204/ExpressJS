a034c513d8a5478ab210f41ab7b982274798be8b
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-04 12:53:14 +0500

    Routing

--- 1-modul/test/index.js
+++ 1-modul/test/index.js
@@ -1,6 +1,19 @@
 import express from "express";
-
+import path, { dirname } from "path"
+import { fileURLToPath } from "url";
 const app = express()
-const PORT = process.env.PORT || 4100
+const __filename = fileURLToPath(
+    import.meta.url)
+const __dirname = dirname(__filename)
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"))
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "about.html"))
+})
+
+const PORT = process.env.PORT || 4100
 app.listen(PORT, () => { console.log("Server is running PORT: ", PORT); })
