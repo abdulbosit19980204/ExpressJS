0b71424404812e10c5fec893381589adf6de15df
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-05 02:13:25 +0500

    Layout

--- 1-modul/5-dars-Layout/index.js
+++ 1-modul/5-dars-Layout/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path, { dirname } from "path"
 import { fileURLToPath } from "url"
-import { engine } from "express-handlebars"
+import { engine, create } from "express-handlebars"
 
 const __filename = fileURLToPath(
     import.meta.url)
@@ -9,19 +9,21 @@ const __dirname = dirname(__filename)
 const app = express()
 
 //Handlebars shu yerda sozlanyapti
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+const hbs = create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 app.set('views', './views');
 
 // Shu yerda handlebars sozlandi
 
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, "views", "index.html"))
     res.render('index')
 
 })
 app.get('/about', (req, res) => {
-    // res.sendFile(path.join(__dirname, "views", "about.html"))
     res.render('about')
 })
 
