c86e91fd1eb334a07abccb5ac5150e01e8f058a8
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-05 07:20:15 +0500

    Layout

--- 1-modul/5-dars-Layout/index.js
+++ 1-modul/5-dars-Layout/index.js
@@ -1,11 +1,7 @@
 import express from "express";
-import path, { dirname } from "path"
-import { fileURLToPath } from "url"
 import { engine, create } from "express-handlebars"
 
-const __filename = fileURLToPath(
-    import.meta.url)
-const __dirname = dirname(__filename)
+
 const app = express()
 
 //Handlebars shu yerda sozlanyapti
