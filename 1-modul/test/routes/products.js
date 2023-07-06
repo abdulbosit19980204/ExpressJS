5b89d4b1d9ec9fdb3c4e68b58f1382adaad77aa0
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-06 14:21:15 +0500

    Dynamic title test

--- 1-modul/test/routes/products.js
+++ 1-modul/test/routes/products.js
@@ -1,13 +1,24 @@
 import { Router } from "express";
 
 const router = Router();
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: "APP | Home"
+    })
+})
 
 router.get('/add', (req, res) => {
-    res.render('add')
+    res.render('add', {
+        title: "APP | Add Products",
+        isAdd: true,
+    })
 })
 
 router.get('/products', (req, res) => {
-    res.render('products')
+    res.render('products', {
+        title: "APP | Products",
+        isProducts: true,
+    })
 })
 
 
