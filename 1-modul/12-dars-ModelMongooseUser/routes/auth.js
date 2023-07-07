17056ac6f7eb84c43abdb8637620c9ad4b91b744
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-08 00:12:17 +0500

    Model Mongoose User

---
+++ 1-modul/12-dars-ModelMongooseUser/routes/auth.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+const router = Router()
+
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: "APP | Register ",
+        isRegister: true,
+    })
+})
+router.get('/login', (req, res) => {
+    res.render('login', {
+        title: "APP | Login",
+        isLogin: true,
+    })
+})
+
+
+export default router
