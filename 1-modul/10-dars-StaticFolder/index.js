da359ec42329d67cb6db8a657b353b121adc96af
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-07 05:27:10 +0500

    Static Folder

--- 1-modul/10-dars-StaticFolder/index.js
+++ 1-modul/10-dars-StaticFolder/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { engine, create } from "express-handlebars"
 import AuthRoutes from "./routes/auth.js"
 import ProductsRoutes from "./routes/products.js"
-const app = express()
+const app = express() // app is exemplar from express
 
 //Handlebars setting
 const hbs = create({
@@ -16,10 +16,11 @@ app.set('views', './views');
 // Handlebars setting is done
 
 // Middlewares
-
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
 app.use(AuthRoutes);
 app.use(ProductsRoutes)
 
+// server is running here
 const PORT = process.env.PORT || 4100
 app.listen(PORT, () => { console.log(`Server is running on port: ${PORT}`); })
