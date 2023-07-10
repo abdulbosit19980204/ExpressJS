070b4a51368411dec1b28851a6ddba0f3b368956
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-11 00:39:31 +0500

    Add product UI

---
+++ 1-modul/20-dars-AddProductUI/routes/products.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: "APP | Apple Store",
+    })
+
+})
+router.get('/add', (req, res) => {
+    res.render('add', {
+        title: "APP | Add products",
+        isAdd: true,
+    })
+})
+router.get('/products', (req, res) => {
+    res.render('products', {
+        title: "APP | Products",
+        isProducts: true,
+    })
+})
+
+export default router
