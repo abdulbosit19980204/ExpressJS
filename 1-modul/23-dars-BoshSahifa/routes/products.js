21257c2f579e7094108ce324ef561d0314c7d72e
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-13 15:27:06 +0500

    Bosh sahifa

--- 1-modul/23-dars-BoshSahifa/routes/products.js
+++ 1-modul/23-dars-BoshSahifa/routes/products.js
@@ -5,9 +5,12 @@ import userMiddleware from "../middleware/user.js";
 
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
+    const products = await Product.find().lean()
+
     res.render('index', {
         title: "APP | Apple Store",
+        products: products,
     })
 
 })
