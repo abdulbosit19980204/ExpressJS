d9e157030889ab8932609cb3106d7bcf58e80869
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-21 04:48:04 +0500

    Add card logics by users

--- 1-modul/ItransitionProject/routes/cartRoutes.js
+++ 1-modul/ItransitionProject/routes/cartRoutes.js
@@ -1,19 +1,33 @@
 import { Router } from "express"
 import CartItem from "../models/Cart.js";
-import Product from "../models/Product.js";
+
 const router = Router()
 
 // Route to add an item to the cart
 router.get('/add-to-cart/:id', async(req, res) => {
     const id = req.params.id
-
-    console.log(id);
-    // const cartProduct = await Product.findById(id).populate('user').lean()
-    // console.log(cartProduct);
     const cartProduct = await CartItem.create({ product: id, user: req.userId, quantity: req.quantity })
-    console.log(cartProduct);
     res.render('myCart', { cartProduct: 'cartProduct', })
-    res.redirect('/')
+    res.redirect('/my-cart')
 });
+
+
+
+router.get('/my-cart', async(req, res) => {
+    try {
+        const user = req.userId ? req.userId.toString() : null
+        const buyProducts = await CartItem.find({ user }).populate('user').populate('product').lean()
+        res.render('myCart', {
+            title: "APP | My Cart",
+            buyProducts: buyProducts.reverse(),
+        })
+    } catch (error) {
+        console.error('Error searching for products:', error);
+        res.redirect('/')
+    }
+
+})
+
+
 
 export default router
--- 1-modul/ItransitionProject/routes/trade.js
+++ 1-modul/ItransitionProject/routes/trade.js
@@ -4,26 +4,6 @@ import Product from "../models/Product.js";
 import CartItem from "../models/Cart.js";
 const router = new Router();
 
-router.get('/my-cart', async(req, res) => {
-    try {
-
-        const buyProducts = await CartItem.find().populate('user').populate('product').lean()
-        console.log(buyProducts);
-        res.render('myCart', {
-            title: "APP | My Cart",
-            buyProducts: buyProducts.reverse(),
-            userId: req.userId ? req.userId.toString() : null,
-            productId: req.productId ? req.id.toString() : null,
-        })
-    } catch (error) {
-        console.error('Error searching for products:', error);
-        //res.status(500).send('Internal Server Error');
-        res.redirect('/')
-    }
-
-})
-
-
 // Post methods
 
 router.post('/my-cart', (req, res) => {
